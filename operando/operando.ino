78072cba74408ec3111ddd2ea175c7e3f8569da4
Mario <malag.oiram@gmail.com>
2018-11-07 19:22:06 -0600

    Se incluyó el Led RGB

--- operando/operando.ino
+++ operando/operando.ino
@@ -4,23 +4,28 @@
 const char * stranger = "StrangerThings:d=4,o=5,b=170:8c4,8p,8c4,8g,8b,8p,8c4,8p,8c4,8g,8e,8p,8c4,8p,8c4,8g,8b,8p,8c4,8p,8c4,8g,8e,8p,8c4,8p,8c4,8g,8b,8p,8c4,8p,8c4,8g,8e,8p,8c4,8p,8c4,8g,8b,8p,8c4,8p,8c4,8g,8e,8p,8p,8e4,8e,8g,8b,8p,8c4,8p,8c4,8g,8e,8p";
 const char * halloween = "Halloween:d=4,o=5,b=180:8d6,8g,8g,8d6,8g,8g,8d6,8g,8d#6,8g,8d6,8g,8g,8d6,8g,8g,8d6,8g,8d#6,8g,8c#6,8f#,8f#,8c#6,8f#,8f#,8c#6,8f#,8d6,8f#,8c#6,8f#,8f#,8c#6,8f#,8f#,8c#6,8f#,8d6,8f#";
 int LED = 6;
-//int Buzzer = 9;
 int Touch = 7;
 int intentos = 0;
+
+//Led RGB
+int Rojo = 4;
+int Verde = 3;
+int Azul = 5;
+//Se almacenan los valores relativos a la intensidad de cada color
+//(las candelas de cada color).
+int mcdRojo = 300;
+int mcdVerde = 1000;
+int mcdAzul = 400;
+//Se realiza una relación entre las candelas de los distintos
+//colores.
+int GradoRojo = (mcdRojo / mcdVerde);
+int GradoVerde = (mcdVerde / mcdVerde);
+int GradoAzul = (mcdAzul / mcdVerde);
 
 const int necessary_delay = 10; //in milliseconds
 bool isTouching = false; //Note how it is declared outside the loop
 unsigned long countStart;
 int kill = 0;
-
-void setup() {
-  pinMode(LED, OUTPUT);
-  pinMode(Buzzer, OUTPUT);
-  pinMode(Touch, INPUT);
-  
-  Serial.begin(115200);
-  Serial.println();  
-}
 
 void loop_touching()
 {
@@ -58,6 +63,36 @@ void loop_touching()
   }
 }
 
+//Colores
+void EstablecerColor(int R, int G, int B) 
+{
+  //Se utiliza la expresión X/GradoX para multiplicar el valor de la
+  //salida PWM por un número que consiga una intensidad de luz
+  //equiparable entre todos los colores.
+  //Se debe tener en cuenta que la salida máxima es 255, por tanto,
+  //la compensación entre los colores se pierde si la relación
+  //X/GradoX genera un valor superior a 255.
+  analogWrite(Rojo, (R / GradoRojo));
+  analogWrite(Verde, (G / GradoVerde));
+  analogWrite(Azul, (B / GradoAzul));
+
+}
+
+
+void setup() 
+{
+  pinMode(LED, OUTPUT);
+  pinMode(Buzzer, OUTPUT);
+  pinMode(Touch, INPUT);
+  pinMode(Rojo, OUTPUT);
+  pinMode(Verde, OUTPUT);
+  pinMode(Azul, OUTPUT);
+  
+  EstablecerColor(0, 200, 0); //RGB
+  
+  Serial.begin(115200);
+  Serial.println();  
+}
 
 void loop() {
   if ( !rtttl::isPlaying() )
@@ -67,6 +102,7 @@ void loop() {
     {
       rtttl::stop();
       rtttl::begin(Buzzer, stranger);
+      EstablecerColor(200, 0, 0); //RGB
       
     }    
   }
